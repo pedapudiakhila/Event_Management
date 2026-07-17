1a8626815fae9670df96fc44b48df87aeff2b217
pedapudiakhila <akhilapedapudi117@gmail.com>
2026-07-17 15:08:48 +0530

    Solved the forgot password issue

--- server/src/utils/sendEmail.js
+++ server/src/utils/sendEmail.js
@@ -1,11 +1,16 @@
 const nodemailer = require('nodemailer')
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_APP_PASSWORD
-  }
+  },
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000
 })
 
 const sendPasswordResetEmail = async (toEmail, resetUrl) => {
